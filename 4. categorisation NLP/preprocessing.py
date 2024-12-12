610735b0f3a6f07c1c194e4601b086e980e7d53f
FrankLloyd83 <ctesch58@gmail.com>
2024-12-12 17:31:40 +0100

    amélioration du preprocessing: filtrage des tokens trop rares et trop fréquents (en nombre de documents où ils sont présents), filtrage des stopwords français et anglais, ajout d'une liste de stopwords pour les éléments html/css

--- 4. categorisation NLP/preprocessing.py
+++ 4. categorisation NLP/preprocessing.py
@@ -4,32 +4,106 @@ Module de prétraitement de texte pour la catégorisation de mails.
 
 import re
 import json
+from collections import Counter
 import nltk
 from nltk.corpus import stopwords
+from nltk.corpus import words
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-# Assurez-vous d'avoir téléchargé les ressources nécessaires
+
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
 nltk.download("punkt_tab")
+nltk.download("words")
 
 
 def preprocess_mails(mails):
     """
-    Prétraite une liste d'emails en nettoyant le texte, en supprimant les 
+    Prétraite une liste d'emails en nettoyant le texte, en supprimant les
     stopwords, en tokenisant et en lemmatisant.
 
     Args:
-        mails (list of dict): Liste de dictionnaires contenant les emails 
+        mails (list of dict): Liste de dictionnaires contenant les emails
         avec les clés "subject" et "body".
 
     Returns:
         list of str: Liste de textes prétraités.
     """
-    stop_words = set(stopwords.words("english"))
+    french_stop_words = set(stopwords.words("french"))
+    english_stop_words = set(stopwords.words("english"))
     lemmatizer = WordNetLemmatizer()
+    english_words = set(words.words("en"))
+    html_stop_words = [
+        "html",
+        "body",
+        "head",
+        "div",
+        "span",
+        "class",
+        "id",
+        "style",
+        "table",
+        "tr",
+        "td",
+        "ul",
+        "ol",
+        "li",
+        "br",
+        "hr",
+        "meta",
+        "link",
+        "script",
+        "none",
+        "block",
+        "inline",
+        "display",
+        "auto",
+        "fixed",
+        "relative",
+        "absolute",
+        "static",
+        "width",
+        "height",
+        "margin",
+        "padding",
+        "border",
+        "color",
+        "background",
+        "font",
+        "size",
+        "align",
+        "top",
+        "bottom",
+        "left",
+        "right",
+        "center",
+        "position",
+        "overflow",
+        "float",
+        "clear",
+        "text",
+        "align",
+        "style",
+        "cursor",
+        "pointer",
+        "important",
+        "screen",
+        "media",
+        "medium",
+        "max",
+        "min",
+        "vh",
+        "vw",
+        "px",
+        "rem",
+        "em",
+        "percent",
+        "z-index",
+    ]
+
+    token_doc_counts = Counter()
 
     for mail in mails:
         # Nettoyer le texte (exemple : enlever les balises HTML et caractères spéciaux)
@@ -40,12 +114,30 @@ def preprocess_mails(mails):
         # Tokenisation
         tokens = word_tokenize(text)
 
-        # Suppression des stopwords, lemmatisation, suppression des mots de moins de 3 caractères et des tokens non alphabétiques
         tokens = [
-            lemmatizer.lemmatize(word) for word in tokens
-            if word.isalpha() and word not in stop_words and len(word) >= 3
+            lemmatizer.lemmatize(word)
+            for word in tokens
+            if word.isalpha()
+            and word not in french_stop_words
+            and word not in english_stop_words
+            and word not in html_stop_words
+            and len(word) >= 3
+            and word in english_words
         ]
 
+        mail["preprocessed"] = " ".join(tokens)
+        token_doc_counts.update(set(tokens))
+
+    min_doc_count = 5
+    max_doc_ratio = 0.5
+
+    for mail in mails:
+        tokens = mail["preprocessed"].split()
+        tokens = [
+            token
+            for token in tokens
+            if min_doc_count < token_doc_counts[token] < max_doc_ratio * len(mails)
+        ]
         mail["preprocessed"] = " ".join(tokens)
 
     # Enregistrer les résultats prétraités dans un fichier JSON
